dcd5a8837be15b9b080a6df7acdcf17bb4957976
Erta <e.ism1@hotmail.com>
2025-12-15 23:22:05 +0100

    ndryshime ne dropdown books

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,22 +20,40 @@ function Header() {
         <ul className="nav-menu">
           <li><NavLink to="/" className="nav-links">HOME</NavLink></li>
           <li><NavLink to="/about" className="nav-links">ABOUT US</NavLink></li>
-         <li className="dropdown">
+      <li className="dropdown mega-dropdown">
   <NavLink to="/books" className="nav-links">
     BOOKS
   </NavLink>
 
-  <ul className="dropdown-menu">
-    <li><NavLink to="/books?category=art">Art</NavLink></li>
-    <li><NavLink to="/books?category=adolescence">Adoleshencë</NavLink></li>
-    <li><NavLink to="/books?category=law">Drejtësi</NavLink></li>
-    <li><NavLink to="/books?category=business">Biznes</NavLink></li>
-    <li><NavLink to="/books?category=leadership">Leadership</NavLink></li>
-    <li><NavLink to="/books?category=science">Shkencë</NavLink></li>
-    <li><NavLink to="/books?category=romance">Romancë</NavLink></li>
-    <li><NavLink to="/books?category=language">Gjuhësi</NavLink></li>
-  </ul>
+  <div className="mega-menu">
+    
+    <div className="mega-column">
+      <h4>Letërsi</h4>
+      <NavLink to="/books?category=art">Art</NavLink>
+      <NavLink to="/books?category=romance">Romancë</NavLink>
+      <NavLink to="/books?category=adolescence">Adoleshent</NavLink>
+      <NavLink to="/books?category=language">Gjuhësi</NavLink>
+    </div>
+
+    <div className="mega-column">
+      <h4>Shkencë & Biznes</h4>
+      <NavLink to="/books?category=science">Shkencë</NavLink>
+      <NavLink to="/books?category=business">Biznes</NavLink>
+      <NavLink to="/books?category=leadership">Leadership</NavLink>
+      <NavLink to="/books?category=law">Drejtësi</NavLink>
+    </div>
+
+    <div className="mega-column">
+      <h4>Të veçanta</h4>
+      <NavLink to="/books?category=history">Histori</NavLink>
+      <NavLink to="/books?category=psychology">Psikologji</NavLink>
+      <NavLink to="/books?category=children">Për fëmijë</NavLink>
+      <NavLink to="/books">Të gjitha →</NavLink>
+    </div>
+
+  </div>
 </li>
+
 
           <li><NavLink to="/authors" className="nav-links">AUTHORS</NavLink></li>
         </ul>
